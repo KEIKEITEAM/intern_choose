32b3a6d9b7679155ce33263193a0dff170d6d576
liang <1039668234@qq.com>
2020-05-29 17:59:17 +0800

    年级、班级、专业、专业群、学生、教师dao层CRUD操作

--- src/main/java/com/lcvc/intern_choose/dao/StudentDao.java
+++ src/main/java/com/lcvc/intern_choose/dao/StudentDao.java
@@ -1,10 +1,41 @@
 package com.lcvc.intern_choose.dao;
 
+import com.lcvc.intern_choose.model.Grades;
 import com.lcvc.intern_choose.model.Student;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentDao extends IBaseDao<Student>{
+    /**
+     * 查询所有Grades
+     * @return 返回Grades对象集合
+     */
+    List<Student> readAll();
 
+    /**
+     * 查询指定的Grades
+     * @param studentNumber
+     * @return 返回查询的结果，1表示成功，0表示失败
+     */
+    Student get(@Param("studentNumber") String studentNumber);
+
+    /**
+     * 添加Grades的方法
+     * @param student
+     * @return 返回添加的结果，1表示成功，0表示失败
+     */
+    int save(Student student);
+
+    /**
+     * 修改Grades的方法
+     * @param student
+     * @return 返回修改的结果，1表示成功，0表示失败
+     */
+    int update(Student student);
+
+    int delete(@Param("studentNumber") String studentNumber);
 
 }
--- src/main/java/com/lcvc/intern_choose/dao/TeacherDao.java
+++ src/main/java/com/lcvc/intern_choose/dao/TeacherDao.java
@@ -1,11 +1,41 @@
 package com.lcvc.intern_choose.dao;
 
 import com.lcvc.intern_choose.model.Teacher;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeacherDao extends IBaseDao<Teacher>{
+    /**
+     * 查询所有Teacher
+     * @return 返回Teacher对象集合
+     */
+    List<Teacher> readAll();
 
+    /**
+     * 查询指定的Teacher
+     * @param teacherNumber
+     * @return 返回查询的结果，1表示成功，0表示失败
+     */
+    Teacher get(@Param("teacherNumber") String teacherNumber);
+
+    /**
+     * 添加Teacher的方法
+     * @param teacher
+     * @return 返回添加的结果，1表示成功，0表示失败
+     */
+    int save(Teacher teacher);
+
+    /**
+     * 修改Teacher的方法
+     * @param teacher
+     * @return 返回修改的结果，1表示成功，0表示失败
+     */
+    int update(Teacher teacher);
+
+    int delete(@Param("teacherNumber") String teacherNumber);
 
 
 }
